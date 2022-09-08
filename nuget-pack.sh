502740ab1f8ab7ca678c75cac206235b0ac6275e
coderweather <coderweather@gmail.com>
2022-09-08 21:32:34 +0300

    updates

--- nuget-pack.sh
+++ nuget-pack.sh
@@ -1,2 +1,2 @@
-dotnet pack src/Generator/Generator.csproj -c Release -o nuget/
-dotnet pack src/ValueObjects/ValueObjects.csproj -c Release -o nuget/
+dotnet pack src/Valueobjects/Generator/Generator.csproj -c Release -o nuget/
+dotnet pack src/Valueobjects/ValueObjects/ValueObjects.csproj -c Release -o nuget/
